e3785562466e8b929750ca946b03e6fc1e8ec885
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2020-04-23 12:15:14 +0200

    chore: fix internal module detection for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,7 @@
 module.exports = {
   extends: "@adfinis-sygroup/eslint-config/ember-app",
   rules: { "ember/no-mixins": "off" },
+  settings: {
+    "import/internal-regex": "^dinacon-awards/",
+  },
 };
