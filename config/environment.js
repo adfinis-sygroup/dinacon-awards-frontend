2f395b32f085d999e27f6686bf693059f737e5b5
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2020-05-01 15:58:15 +0200

    feat: use ember-router-scroll

--- config/environment.js
+++ config/environment.js
@@ -7,6 +7,7 @@ module.exports = function (environment) {
     rootURL: "/",
     locationType: "auto",
     podModulePrefix: "dinacon-awards/ui",
+    historySupportMiddleware: true,
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
